a96ebf31a7b88a5ee8740cebab29988c6697d513
ArekaYash <yashmain31@gmail.com>
2024-07-17 18:03:15 +0530

    Finalized code

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 /*  >> MODAL WINDOW  */
 
-let modalContent = document.querySelector(".modal-content");
-let openModal = document.querySelector(".open-modal");
-let closeModal = document.querySelector(".close-modal");
+let openModal = document.querySelector(".openModal");
+let openModal2 = document.querySelector(".openModal2");
+let closeModal = document.querySelectorAll(".closeModal");
 let blurBg = document.querySelector(".blur-bg");
 
 openModal.addEventListener("click", function () {
-  modalContent.classList.remove("hidden-modal");
+  document.getElementById("passwordModal").classList.remove("hiddenModal");
   blurBg.classList.remove("hidden-blur");
 });
 
+openModal2.addEventListener("click", function () {
+  document.getElementById("strengthModal").classList.remove("hiddenModal");
+  blurBg.classList.remove("hidden-blur");
+
+  // Reset the input field for strength password
+  document.getElementById("strengthPassword").value = "";
+});
+
 let closeModalFunction = function () {
-  modalContent.classList.add("hidden-modal");
+  document.querySelectorAll(".modalContent").forEach((modal) => {
+    modal.classList.add("hiddenModal");
+  });
   blurBg.classList.add("hidden-blur");
+
+  // Reset the input field for strength password when closing strength modal
+  document.getElementById("strengthPassword").value = "";
 };
 
 blurBg.addEventListener("click", closeModalFunction);
-closeModal.addEventListener("click", closeModalFunction);
+closeModal.forEach((btn) => btn.addEventListener("click", closeModalFunction));
 
 document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape" && !modalContent.classList.contains("hidden")) {
+  if (event.key === "Escape") {
     closeModalFunction();
   }
 });
@@ -29,8 +42,9 @@ document.addEventListener("keydown", function (event) {
 var password = document.getElementById("password");
 
 function genPassword() {
+  resetCopyButton();
   var chars =
-    "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    "0123456789abcdefghijklmnopqrstuvwxyz_!@#$%^&*()<>?[]{}|+ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var passwordLength = 10;
   var password = "";
   for (var i = 0; i <= passwordLength; i++) {
@@ -42,7 +56,66 @@ function genPassword() {
 
 document.getElementById("password").value = password;
 
-function copyPassword() {
+async function copyPassword() {
+  copyPassword1();
+  copyPassword2();
+}
+
+async function copyPassword1() {
+  const copyButton = document.getElementById("button");
+  copyButton.innerHTML = "Copied !";
+}
+
+async function copyPassword2() {
   var copyText = document.getElementById("password");
   navigator.clipboard.writeText(copyText.value);
+}
+
+async function resetCopyButton() {
+  const copyButton = document.getElementById("button");
+  copyButton.innerHTML = "Copy";
+}
+
+/*  >> PASSWORD STRENGTH CHECKER  */
+
+function testStrength() {
+  console.log("Test strength button clicked"); // Opens modal window of "Test Strength"
+}
+
+function checkStrength() {
+  var strengthPassword = document.getElementById("strengthPassword").value;
+
+  // Regular expressions to check for each requirement
+  var lengthRegex = /.{8,}/; // 8 characters
+  var spaceRegex = /\s/; // spaces
+  var capitalRegex = /[A-Z]/; // capital letter
+  var specialRegex = /[!@#$%^&*()_+[\]{}\\|,.<>/?]/; // special symbol
+
+  // Check each condition
+  var isLengthValid = lengthRegex.test(strengthPassword);
+  var hasNoSpaces = !spaceRegex.test(strengthPassword);
+  var hasCapitalLetter = capitalRegex.test(strengthPassword);
+  var hasSpecialSymbol = specialRegex.test(strengthPassword);
+
+  // Prepare feedback message
+  var feedbackMessage = "Passwords should have:\n";
+  if (!isLengthValid) {
+    feedbackMessage += "- At least 8 characters\n";
+  }
+  if (!hasNoSpaces) {
+    feedbackMessage += "- No spaces\n";
+  }
+  if (!hasCapitalLetter) {
+    feedbackMessage += "- At least one capital letter\n";
+  }
+  if (!hasSpecialSymbol) {
+    feedbackMessage += "- At least one special symbol\n";
+  }
+
+  // Determine the strength based on conditions
+  if (isLengthValid && hasNoSpaces && hasCapitalLetter && hasSpecialSymbol) {
+    alert("Password strength: Strong");
+  } else {
+    alert("Password strength: Weak\n\n" + feedbackMessage);
+  }
 }
